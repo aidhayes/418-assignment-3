a5245e010ab49d1b3620735bbb5d9be2ee36c6f8
aidhayes <aid.hayes01@gmail.com>
2022-10-04 18:43:37 -0400

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express') // IMPORT express
 const app = express() // create an instance of the import.
-const port = 3000 // variable to store the port to listen on
+const port = 9000 // variable to store the port to listen on
 
 // Display HTML to localhost
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html")) 
