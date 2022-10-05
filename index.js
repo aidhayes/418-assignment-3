907db9fcb3d694931f7a365adab9f4c7dbaef4e4
aidhayes <aid.hayes01@gmail.com>
2022-10-04 21:33:40 -0400

    Added html to ejs file then render it in index.js

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 const express = require('express') // Import express
 const app = express() // Create an instance of the import
 const port = 9000 // Variable to store the port to listen on
+var path = require('path');
 
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // Display HTML to localhost
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html")) 
+app.use(express.static(path.join(__dirname, 'index')));
+
+app.get('/', (req, res) => res.render('index', {}));
 
 // Make app listen on the port
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
